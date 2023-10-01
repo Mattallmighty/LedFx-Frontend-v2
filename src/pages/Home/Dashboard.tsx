eb56b56196016108fdcb57d9118fd3cbb24c7e81
Matt Muller <matt4788667@hotmail.com>
2023-10-01 18:03:57 +1100

    Visualiser folder

--- src/pages/Home/Dashboard.tsx
+++ src/pages/Home/Dashboard.tsx
@@ -25,7 +25,7 @@ import Popover from '../../components/Popover/Popover'
 import TourHome from '../../components/Tours/TourHome'
 import SmartBar from '../../components/Dialogs/SmartBar'
 import MGraph from '../../components/MGraph'
-import Visualiser from '../../components/Visualiser'
+import Visualiser from '../../components/Visualiser/Visualiser'
 
 const Dashboard = () => {
   const theme = useTheme()
